2d0233c6d5ce203b6e08705dc1af4d5c191ce50b
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-26 16:16:55 +0700

    method for inputting array and accepting user input

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -14,5 +14,30 @@ namespace SearchAlgorithm
         int n;
         //get the number of elements to store in the array
         int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have 1 minimum and maximum 20 elements. \n");
+            }
+            //accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<"+(i+1)+">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
