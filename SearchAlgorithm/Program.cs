8fc623905c5668535d294ca3cb7e4e32496b3b54
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-26 16:07:31 +0700

    declare variable arr 20, variable n, variable i

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //array to be searched
+        int[] arr = new int[20];
+        //number of elements in the array
+        int n;
+        //get the number of elements to store in the array
+        int i;
+    }
+}
