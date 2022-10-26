d432f4fec311dc2f967e635f2dd0b25b84a61c4d
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-26 17:02:24 +0700

    Add method for linear search

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -78,5 +78,34 @@ namespace SearchAlgorithm
                 ch = char.Parse(Console.ReadLine().ToUpper());
             } while ((ch == 'y'));
         }
+
+        public void LinearSearch()
+        {
+            char ch;
+            //search for number of comparison
+            int ctr;
+            do
+            {
+                //accept the number to be searched
+                Console.WriteLine("\nEnter the element you want to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + " found at position " + (i + 1).ToString());
+                        break;
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + " not found in the array ");
+                Console.WriteLine("\n Number of comparison: " + ctr);
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
+        }
     }
 }
